2787d72a33e55f4774cebad7926b93cba5ebd4a6
email <vassili642@gmail.com>
2022-11-21 16:02:41 +0300

    Finish task 6

--- Program.cs
+++ Program.cs
@@ -18,26 +18,23 @@ for (int i = 0; i < arr.Length; i++)
     }
 }
  
-Console.WriteLine($"The number of integers: {count}");*/
+Console.WriteLine($"The number of integers greater than 0: {count}");*/
 
 /*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
 
 Console.Write("Enter b1: ");
-var b1 = Convert.ToDouble(Console.ReadLine());
+double b1 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Enter k1: ");
-var k1 = Convert.ToDouble(Console.ReadLine());
+double k1 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Enter b2: ");
-var b2 = Convert.ToDouble(Console.ReadLine());
+double b2 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Enter k2: ");
-var k2 = Convert.ToDouble(Console.ReadLine());
+double k2 = Convert.ToDouble(Console.ReadLine());
  
  
-var x = -(b1 - b2) / (k1 - k2);
-var y = k1 * x + b1;
- 
-x = Math.Round(x, 3);
-y = Math.Round(y, 3);
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
  
 Console.WriteLine($"Intersection at a point: ({x}; {y})");
