3b8e0d7277c47dafade0fc327dcddeb253cb5cbe
email <vassili642@gmail.com>
2022-11-21 19:21:11 +0300

    Finish all

--- Program.cs
+++ Program.cs
@@ -6,25 +6,63 @@
 1, -7, 567, 89, 223 -> 4*?*/
 
 
-/*Console.Write("Enter integers with space: ");
-int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-int count = 0;
- 
-for (int i = 0; i < arr.Length; i++)
+Console.Write("Enter numbers separated by commas and spaces: ");
+int[] numbers = StringToNum(Console.ReadLine());
+int sum = 0;
+for (int i = 0; i < numbers.Length; i++)
 {
-    if (arr[i] > 0)
+    if (numbers[i] > 0)
     {
-        count++;
+        sum++;
     }
 }
- 
-Console.WriteLine($"The number of integers greater than 0: {count}");*/
+Console.WriteLine();
+Console.WriteLine($"The number of integers greater than 0 = {sum}");
+
+
+int[] StringToNum(string input)
+{
+    int count = 1;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == ',')
+        {
+            count++;
+        }
+    }
+
+    int[] numbers = new int [count];
+    int index = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temp = "";
+
+        while (input [i] != ',')
+        {
+        if(i != input.Length - 1)
+        {
+            temp += input [i].ToString();
+            i++;
+        }
+        else
+        {
+            temp += input [i].ToString();
+            break;
+        }
+        }
+        numbers[index] = Convert.ToInt32(temp);
+        index++;
+    }
+    return numbers;
+}
+
 
 /*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
 
-Console.Write("Enter b1: ");
+/*Console.Write("Enter b1: ");
 double b1 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Enter k1: ");
 double k1 = Convert.ToDouble(Console.ReadLine());
@@ -37,4 +75,6 @@ double k2 = Convert.ToDouble(Console.ReadLine());
 double x = (-b2 + b1)/(-k1 + k2);
 double y = k2 * x + b2;
  
-Console.WriteLine($"Intersection at a point: ({x}; {y})");
+Console.WriteLine($"Intersection at a point: ({x}; {y})");*/
+
+
